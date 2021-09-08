a5cbf6153285e959791c4c043789671904152251
qbaccon <qbaccon@hotmail.com>
2021-09-08 13:51:22 +0200

    update get fct

--- index.php
+++ index.php
@@ -1,30 +1,39 @@
 <?php
-$url = 'https://mhw-db.com/monsters';
-$response = file_get_contents('https://mhw-db.com/monsters');
-$json = json_decode($response);
-$id = $json[0]->id;
-
-// utiliser les routes existantes
-// $response2 = file_get_contents('https://mhw-db.com/monsters' . '/' . $id);
-// $json2 = json_decode($response2);
-// var_dump($json2->name);
-
-?>
-    <form action="getMonster.php?id=<?= $id; ?>" method="get">
-        <input type="submit">
-
-    </form>
-<?php
-
-// foreach($json as $tab) {
-//     foreach($tab as $key => $value) {
-//         var_dump($key, ' : ', $value, '<br>');
-//     }
-//     echo '</br>';
-// }
-
-
-// writeln
-
-
-
+// $url = 'https://mhw-db.com/monsters';
+$pdo = new PDO("mysql:host=localhost;dbname=prj_api;charset=utf8","root","");
+if (isset($_GET['get']))
+{
+	if ($_GET['get'] == 'monsters')
+	{
+		// $response = file_get_contents('https://mhw-db.com/monsters');
+		$json = $pdo->query("SELECT * FROM monsters");
+		$json = $json->fetchAll();
+		foreach ($json as $elm)
+		{
+			if ($elm['id'] < 10)
+				echo '0';
+			echo $elm['id'] . ': ';
+			echo $elm['name_en'];
+			echo "</br>";
+		}
+		$json = json_encode($json);
+		// var_dump($json);
+		// $json = json_decode($json);
+		// var_dump($json);
+	}
+	else if ($_GET['get'] == 'weapons')
+	{
+		$response2 = file_get_contents('https://mhw-db.com/weapons');
+		$json2 = json_decode($response2);
+		var_dump ($json2);
+		// foreach ($json as $elm)
+		// {
+		// 	if ($elm->id < 10)
+		// 		echo '0';
+		// 	echo $elm->id . ': ';
+		// 	echo $elm->name;
+		// 	echo "</br>";
+		// }
+		// $json = json_encode($response);
+	}
+}
