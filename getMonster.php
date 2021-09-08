ab02d15c7ac631ac4679fab6503d0f4cd63a9964
qbaccon <qbaccon@hotmail.com>
2021-09-08 13:54:42 +0200

    refresh

--- getMonster.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_GET['id'])) {
-    echo $_GET['id'];
-}
-// function getMonster($url, $id) {
-//     $response = file_get_contents($url . '/' . $id);
-//     var_dump(json_decode($response));
-// }
