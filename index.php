9e2ae3bcdb83f83d944c76a150945f3347803d13
qbaccon <qbaccon@hotmail.com>
2021-09-08 14:40:01 +0200

    upll fo test fct

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ if (isset($_GET['get']))
 	if ($_GET['get'] == 'monsters')
 	{
 		// $response = file_get_contents('https://mhw-db.com/monsters');
-		$json = $pdo->query("SELECT * FROM monsters");
-		$json = $json->fetchAll();
-		foreach ($json as $elm)
+		$res = $pdo->query("SELECT * FROM monsters");
+		$res = $res->fetchAll();
+		foreach ($res as $elm)
 		{
 			if ($elm['id'] < 10)
 				echo '0';
@@ -16,24 +16,32 @@ if (isset($_GET['get']))
 			echo $elm['name_en'];
 			echo "</br>";
 		}
-		$json = json_encode($json);
+		$json = json_encode($res);
 		// var_dump($json);
 		// $json = json_decode($json);
 		// var_dump($json);
 	}
 	else if ($_GET['get'] == 'weapons')
 	{
-		$response2 = file_get_contents('https://mhw-db.com/weapons');
-		$json2 = json_decode($response2);
-		var_dump ($json2);
-		// foreach ($json as $elm)
-		// {
-		// 	if ($elm->id < 10)
-		// 		echo '0';
-		// 	echo $elm->id . ': ';
-		// 	echo $elm->name;
-		// 	echo "</br>";
-		// }
+		$res = $pdo->query("SELECT * FROM weapons");
+		$res = $res->fetchAll();
+		// var_dump($res);
+		foreach ($res as $elm)
+		{
+			if ($elm['id'] < 10)
+				echo '0';
+			echo $elm['id'] . ': ';
+			echo $elm['name_en'];
+			echo ' (' . $elm['weapon_type'] . ') ';
+			echo $elm['previous_en'];
+		}
 		// $json = json_encode($response);
 	}
+}
+
+function getMonsters()
+{
+	$res = $pdo->query("SELECT * FROM monsters");
+	$res = $res->fetchAll();
+	$json = json_encode($res);
 }
