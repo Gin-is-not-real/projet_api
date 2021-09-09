888383df077962d33aaba70a42e57ba0e24dc431
Gin-is-not-real <ninapariat@live.fr>
2021-09-09 15:34:24 +0200

    add form-filter-monster on monsters/index.php, add js script file for generate input or select  depending on availables options for each fields

--- application/monsters/index.php
+++ application/monsters/index.php
@@ -1,4 +1,56 @@
 <?php
-// $monsters = json_decode(file_get_content(""));
+$monsters = json_decode(file_get_contents("http://localhost/FOLDERS/FORM_PROJETS/form_projet_api/projet_api/api/monsters"));
+ob_start();
+?>
 
-// ob_start()
+<article>
+    <div>
+        <form action="" name="form-filter-monsters" >
+        <div style="display: flex">
+            <div>
+                <label for="select-field">field: </label>
+                <select name="select-field" id="select-field">
+                    <option value="name_en" selected>nom</option>
+                    <option value="ecology_en">ecologie</option>
+                    <option value="size">taille</option>
+                    <option value="pitfall_trap">pitfall trap</option>
+                    <option value="shock_trap">shock trap</option>
+                    <option value="vine_trap">vine trap</option>
+                </select>
+            </div>
+            <div class="adaptativ-input-container">
+                <label for="inp-search">valeur: </label>
+                <input type="text">
+            </div>
+        </div>
+
+        <div>
+            <input type="submit">
+        </div>
+        </form>
+    </div>
+<?php
+foreach($monsters as $elt) {
+?>
+    <figure style="border: 1px solid black; width: 150px; display: flex; flex-direction: column; align-items: center" >
+        <figcaption style="background-color: #80808091; text-align: center; width: 100%">
+            <?= $elt->name_en ?>
+        </figcaption>
+        <img src="../public/images/monsters/<?= $elt->id ?>.png" width="100px">
+    </figure>
+
+    <?php
+
+    foreach($elt as $key => $value) {
+        // echo $key . ': ' .intval($key) . '<br>';
+        echo $key . ': ' . $value . '<br>';
+    }
+};
+
+?>
+</article>
+
+
+<?php
+$content = ob_get_clean();
+require_once('../template.php');
---
+++ application/public/scripts/js/script.js
@@ -0,0 +1,65 @@
+let selectFields = document.querySelector('#select-field');
+let adaptativInputContainer = document.querySelector('.adaptativ-input-container');
+//ce tableau defini quels champs doivent aussi generer un select, et leurs options
+let subSelects = [];
+subSelects['ecology_en'] = [
+    {"value": "relict", "text": "relict"},
+    {"value": "piscine wyvern", "text": "piscine wyvern" },
+    {"value": "flying wyvern", "text": "flying wyvern" },
+    {"value": "fanged wyvern", "text": "fanged wyvern" },
+    {"value": "fanged beast", "text": "fanged beast" },
+    {"value": "elder dragon", "text": "elder dragon" },
+    {"value": "brute wyvern", "text": "brute wyvern" },
+    {"value": "bird wyvern", "text": "bird wyvern" },
+];
+subSelects['size'] = [
+    {"value": "small", "text": "small"},
+    {"value": "large", "text": "large"},
+];
+subSelects['pitfall_trap'] = [
+    {"value": "true", "text": "oui"},
+    {"value": "", "text": "non"},
+];
+subSelects['shock_trap'] = subSelects['pitfall_trap'];
+subSelects['vine_trap'] = subSelects['pitfall_trap'];
+
+
+console.log(selectFields);
+
+selectFields.addEventListener('change', function() {
+    if(adaptativInputContainer.childNodes[3] != undefined) {
+        console.log('il y a un childnode');
+        adaptativInputContainer.childNodes[3].remove();
+    }
+
+    let input;
+    if(subSelects[this.value] != undefined) {
+        input = createSelect(subSelects[this.value]);
+    }
+    else {
+        input = createInput();
+    }
+
+    adaptativInputContainer.appendChild(input);
+})
+function createInput() {
+    let input = document.createElement('input');
+    input.type = "text";
+    input.name = "inp-search";
+    return input;
+}
+
+//genere un select et ses option
+function createSelect(array) {
+    let select = document.createElement('select');
+    select.name = "inp-search";
+
+    array.forEach(element => {
+        let option = document.createElement('option');
+        option.value = element.value;
+        option.textContent = element.text;    
+        select.appendChild(option);
+    });
+
+    return select;
+}
