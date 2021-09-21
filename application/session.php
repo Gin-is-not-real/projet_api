23f7964c47052c00b0543cc4952f938bd8a2f37a
Gin-is-not-real <ninapariat@live.fr>
2021-09-21 15:29:46 +0200

    test gitignore

--- application/session.php
+++ application/session.php
@@ -3,3 +3,4 @@ if(session_id() == '') {
     session_start();
 }
 $_SESSION['base-url'] = 'http://localhost/ACS_project/projet_api/';
+$_SESSION['base-url'] = 'http://localhost/FOLDERS/FORM_PROJETS/form_projet_api/';
